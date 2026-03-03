81b80447d1643372379340506f40d2df974f0156
Dikra Nkairi <dikrankairi@gmail.com>
2026-03-03 10:57:41 +0100

    Ajout collection Bruno TD01

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-export const app = new Hono()
+export const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.json({
+    success: true,
+    message: process.env.API_NAME ?? "World Cup Ticketing API",
+  });
+});
+
+app.get("/health", (c) => {
+  return c.json({
+    success: true,
+    message: process.env.API_NAME ?? "World Cup Ticketing API",
+    uptime: process.uptime(),
+    environment: process.env.NODE_ENV ?? "dev",
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { app } from "./app";
 
 export default {
-  port: 3000,
+  port: Number(process.env.PORT ?? 3000),
   fetch: app.fetch,
 };
